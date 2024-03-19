c23bb4f949a3e21272165ce37575921b4fb7124b
Elisa Edson <elisaedson@Elisas-MacBook-Pro.local>
2024-03-19 11:50:46 +0000

    check modal recognises and replaces important tags

--- src/javascripts/modules/components/infoPanel.js
+++ src/javascripts/modules/components/infoPanel.js
@@ -1,3 +1,4 @@
+import { Modal } from '@zendeskgarden/react-modals'
 import React, { createElement } from 'react'
 
 export default function infoPanel({body}) {
@@ -5,6 +6,7 @@ export default function infoPanel({body}) {
   let display
   let mappedBody = []
 
+  //Linearly processes body and converts to appropriate JSX
   body.map(key => (
     console.log(`my name is key: ${key}`),
     createHeader(key[0], true),
@@ -28,8 +30,9 @@ export default function infoPanel({body}) {
   } 
   //key value array into list
   function createList(list){
-    
+
     let listItems = list.map(item => (
+      item = checkModal(item),
       <li>{item}</li>
     ))
 
@@ -39,13 +42,26 @@ export default function infoPanel({body}) {
   }
   //key value string/number into article
   function createArticle(article){
+
+    article = checkModal(article)
+
     return(
       mappedBody.push(<article>{article}</article>)
     )
   }
-  //key value string with important flag into modal
-  function createModal(){
+  //key value string check for important flag then create modal
+  function checkModal(modal){
 
+    if (!isNaN(modal)){
+      return modal
+    }
+    console.log(`i am the modal value and my name is ${modal}`)
+    if (modal.startsWith("!important")){
+      modal = modal.replace("!important", "")
+      console.log("modal code goes here")
+    }
+    console.log("another successful modal")
+    return modal
   }
 
   display = createElement('div', {},
