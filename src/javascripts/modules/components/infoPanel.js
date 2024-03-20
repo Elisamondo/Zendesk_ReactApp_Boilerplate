ac5316a14b9e6bfc1e7e4203ac80899a3b4283a6
Elisa Edson <elisaedson@Elisas-MacBook-Pro.local>
2024-03-20 15:40:24 +0000

    build body function allows for important content to display first

--- src/javascripts/modules/components/infoPanel.js
+++ src/javascripts/modules/components/infoPanel.js
@@ -5,6 +5,8 @@ export default function infoPanel({body}) {
   console.log(body)
   let display
   let mappedBody = []
+  let cache = []
+  let importantFlag = false
 
   //Linearly processes body and converts to appropriate JSX
   body.map(key => (
@@ -15,42 +17,46 @@ export default function infoPanel({body}) {
       createHeader(content[0], false),
       
       content[1] instanceof Array ? createList(content[1]) : 
-      createArticle(content[1])
-    ))
+      createArticle(content[1]),
+
+      buildBody()
+    )),
+    console.log("i'm about to body build")
   ))
 
   //key pair name into heading
   function createHeader(header, topLevel){
+    checkImportant(header)
     if (topLevel){
-      mappedBody.push(<h2>{header}</h2>)
+      cache.push(<h2>{header}</h2>)
     }
     else{
-      mappedBody.push(<h3>{header}</h3>)
+      cache.push(<h3>{header}</h3>)
     }
   } 
   //key value array into list
   function createList(list){
 
     let listItems = list.map(item => (
-      item = checkModal(item),
+      item = checkImportant(item),
       <li>{item}</li>
     ))
 
     return(
-      mappedBody.push(<ul>{listItems}</ul>)
+      cache.push(<ul>{listItems}</ul>)
     )
   }
   //key value string/number into article
   function createArticle(article){
 
-    article = checkModal(article)
+    article = checkImportant(article)
 
     return(
-      mappedBody.push(<article>{article}</article>)
+      cache.push(<article>{article}</article>)
     )
   }
   //key value string check for important flag then create modal
-  function checkModal(modal){
+  function checkImportant(modal){
 
     if (!isNaN(modal)){
       return modal
@@ -58,10 +64,17 @@ export default function infoPanel({body}) {
     console.log(`i am the modal value and my name is ${modal}`)
     if (modal.startsWith("!important")){
       modal = modal.replace("!important", "")
-      console.log("modal code goes here")
+      importantFlag = true
     }
     console.log("another successful modal")
     return modal
+  }
+
+  function buildBody(){
+    console.log("body building")
+    importantFlag ? (mappedBody.unshift(cache)) : mappedBody.push(cache)
+    cache = []
+    importantFlag = false
   }
 
   display = createElement('div', {},
