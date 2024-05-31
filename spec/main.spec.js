13f14631de450cdf3e3c8da35ff3a238c762947a
Elisa Edson <elisaedson@Elisas-MacBook-Pro.local>
2024-05-31 14:11:13 +0100

    comments

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -4,14 +4,17 @@ import React from "react"
 
 describe("main div", () => {
   let body
+  //mounts component in test environment
   beforeEach(() => {
     body = render(<Main/>)
   })
 
+  //wipes test environment for clean testing
   afterEach(() => {
     cleanup
   })
-  it("retrieve main div without App container", () => {
+
+  it("retrieve main component without App container", () => {
     expect(body).toBeTruthy()
   })
 })
