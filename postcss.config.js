3f2a2ae595e490ebbc083c460cd4b6b5628718ec
Elisa Edson <elisaedson@Elisas-MacBook-Pro.local>
2024-10-21 12:40:50 +0100

    tailwind integration fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  plugins: [
-    require('postcss-preset-env'),
-    require('postcss-import')
-  ]
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    autoprefixer: {},
+    "postcss-inline-svg": {
+      "path": "node_modules/@zendeskgarden/svg-icons/src"
+    }
+  }
 }
+
